625f49ac14edb25268f95c3e7c24456539196413
Carlos Daniel Cañedo García <carlosdany58@hotmail.com>
2020-06-19 21:40:41 -0500

    Se añade servidor NodeJS con Angular en client

---
+++ index.js
@@ -0,0 +1,30 @@
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
+const bodyParser = require("body-parser");
+const express = require("express");
+const admin = require("firebase-admin");
+
+const csrfMiddleware = csrf({ cookie: true });
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.engine("html", require("ejs").renderFile);
+app.use(express.static("static"));
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(csrfMiddleware);
+
+app.all("*", (req, res, next) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    next();
+});
+
+app.get("/", function (req, res) {
+    res.render("index.html");
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+});
