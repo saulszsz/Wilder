3b1429680e3cfbdcc91aae5b3af9f23dcb2cca88
saulszsz <saul_szsz@hotmail.com>
2020-06-24 16:09:27 -0500

    Se agrega pagina home libre

--- client/src/app/components/inicio/inicio.component.ts
+++ client/src/app/components/inicio/inicio.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FireService } from '../../services/fire.service';
+import { map, switchMap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-inicio',
@@ -6,8 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+  uid = this._fs._afAuth.authState.pipe(
+    map(authState => {
+      if (!authState) {
+        return '';
+      } else {
+        return authState.uid;
+      }
+    })
+  );
+  user = this.uid.pipe(
+    switchMap(uid => {
+      if (!uid) {
+        return of(false);
+      } else {
+        return this._fs.getUser(uid);
+      }
+    }),
+  );
 
-  constructor() { }
+  constructor(private _fs: FireService) { }
 
   ngOnInit(): void {
   }
