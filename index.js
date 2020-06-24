a8f89d450365d350752a6b9122633b088cbffb89
Carlos Daniel Cañedo García <carlosdany58@hotmail.com>
2020-06-23 23:43:06 -0500

    Listo para Heroku

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const admin = require("firebase-admin");
 var firebase = require("firebase");
 const path = require('path');
 
-const serviceAccount = require('./serviceAccountKey.json');
 const firebaseConfig = {
     apiKey: "AIzaSyDPjgrScq2uCDySCQA-59J_K3a1-KM0LDc",
     authDomain: "wilderinv.firebaseapp.com",
@@ -19,7 +18,18 @@ const firebaseConfig = {
 }
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+        type: process.env.FB_TYPE,
+        project_id: process.env.FB_PROJECT_ID,
+        private_key_id: process.env.PRIVATE_KEY_ID,
+        private_key: process.env.PRIVATE_KEY,
+        client_email: process.env.CLIENT_EMAIL,
+        client_id: process.env.CLIENT_ID,
+        auth_uri: process.env.AUTH_URI,
+        token_uri: process.env.TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.AUTH_PROVIDER,
+        client_x509_cert_url: process.env.CLIENT_X,
+    }),
     databaseURL: "https://wilderinv.firebaseio.com",
 });
 
