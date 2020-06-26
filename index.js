f941ac383f982bbbdf8d6b786cab3a1d0ff14cf2
Angel Mosqueda <Angel_Mosqueda_Esparza@hotmail.com>
2020-06-26 18:57:13 -0500

    En contact ya se envian los correos de contacto, y ese formulario requiere ser llenado para poder enviarse

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cookieParser = require("cookie-parser");
 const csrf = require("csurf");
 const bodyParser = require("body-parser");
-const express = require("express");
+const express = require('express');
 const admin = require("firebase-admin");
 var firebase = require("firebase");
 const path = require('path');
@@ -45,6 +45,58 @@ app.use(cookieParser());
 app.engine("html", require("ejs").renderFile);
 app.use(express.static("static"));
 app.use(express.static("client/dist/Wilder"));
+
+//Cosas necesarias para enviar el formulario de contact
+app.use(express.urlencoded({extended: false})); //Sirve para entender los datos que lleguen del formulario y que no salga como undefined
+app.use(express.json());//Hace que el servidor entienda datos en formatos JSON
+
+require('dotenv').config(); 
+const nodemailer = require('nodemailer');
+
+app.post('/enviar_contacto', (req,res) => {
+    var body=req.body;     //console.log(req.body);
+    contentHTML = `
+        <h1>Informacion del Usuario</h1>
+        <ul>
+        <li>Nombre: ${body.name}</li>
+        <li>Email: ${body.mail}</li>
+        <li>Telefono: ${body.phone}</li>
+        </ul>
+        <p>Mensaje: ${body.message}</p>
+    `;
+                                                            //console.log(contentHTML);    
+    //Step 1
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth:{
+            user:'WilderApiISC@gmail.com',
+            pass:'Hola1234'
+        }
+    });
+
+    //Step 2
+    let mailOptions={
+        from: '',
+        to:'WilderApiISC@gmail.com',
+        subject:'Mensaje de Contacto',
+        html:contentHTML
+        //text:''+contentHTML
+    };
+
+    //Step 3
+    transporter.sendMail(mailOptions, function (err,data){
+        if(err){
+            console.log('Error');
+        }else{
+            console.log('Succes');
+        }
+    });
+
+    res.send('/succes.html');
+});
+
+
+//Fin de Cosas necesarias para enviar el formulario de contact
 
 app.use(bodyParser.json());
 app.use(csrfMiddleware);
