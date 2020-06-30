7b5a0f7027e92074c37766d37fe7f4934e3763e5
Angel Mosqueda <Angel_Mosqueda_Esparza@hotmail.com>
2020-06-29 22:44:14 -0500

    Se pusieron los datos de articulos sacados de la base de datos de Firebase en la grafica

--- client/src/app/components/graphics/graphics.component.ts
+++ client/src/app/components/graphics/graphics.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FireService } from '../../services/fire.service';
+import { Router } from '@angular/router';
+import { map, startWith, switchMap } from 'rxjs/operators';
+
+import { FormControl } from '@angular/forms';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-graphics',
@@ -11,9 +17,66 @@ export class GraphicsComponent implements OnInit {
   public doughnutChartData = [120, 150, 180];
   public doughnutChartType = 'doughnut';
 
-  constructor() { }
+  //Cosas Añadidas
+  myControl = new FormControl();
+  filteredOptions: Observable<string[]>;
+  uid = this._fs._afAuth.authState.pipe(
+    map(authState => {
+      if (!authState) {
+        return '';
+      } else {
+        return authState.uid;
+      }
+    })
+  );
+  user: any = this.uid.pipe(
+    switchMap(uid => {
+      if (!uid) {
+        this._router.navigate(['/']);
+      } else {
+        return this._fs._fireDb.object('users/' + uid).valueChanges();
+      }
+    }),
+  );
+
+  constructor(private _fs: FireService, private _router: Router) { }
+    
+  activos = [];
+  activos_etq = [];
+  activos_numero = [];
 
   ngOnInit(): void {
+
+    this._fs.getActivos().subscribe(
+      (result) => {
+        if (result) {
+          var array = [];
+          for (let i in result) {
+            array.push(result[i]);
+            array[array.length - 1]['id'] = i;
+          }
+        }
+        array.forEach((dato) => {
+          this.activos.push(dato);
+        });
+        for(let i = 0; i < this.activos.length; i++) {
+          if(!this.activos_etq.includes(this.activos[i].categoria)){
+            this.activos_etq.push(this.activos[i].categoria);
+            this.activos_numero.push(0);
+          }
+          this.activos_numero[this.activos_etq.indexOf(this.activos[i].categoria)] ++;
+        }
+        console.log(this.activos_numero);
+        console.log(this.activos_etq);
+        this.doughnutChartLabels = this.activos_etq;
+        this.doughnutChartData = this.activos_numero;
+      },
+      (error) => {
+        return [];
+      }
+    );
+
   }
+
 
 }
