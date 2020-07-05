0e26a4f7a82a4da8b5fbb9bcd18c455ae3f9853f
Angel Mosqueda <Angel_Mosqueda_Esparza@hotmail.com>
2020-07-05 13:29:13 -0500

    Agregue una grafica que muestra los productos registrados en cada mes

--- client/src/app/components/graphics/graphics.component.ts
+++ client/src/app/components/graphics/graphics.component.ts
@@ -20,6 +20,34 @@ export class GraphicsComponent implements OnInit {
   public doughnutChartData = [120, 150, 180];
   public doughnutChartType = 'doughnut';
 
+  /*public steppedChartLabels = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+  public steppedChartData = [];
+  public steppedChartType = 'stepped';*/
+
+  public barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true,
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(153, 102, 255, 0.2)'
+    ]
+  };
+  public barChartLabels = ['Enero-2020','Febrero-2020','Marzo-2020','Abril-2020','Mayo-2020','Junio-2020','Julio-2020','Agosto-2020','Septiembre-2020','Octubre-2020','Noviembre-2020','Diciembre-2020'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+  public barChartData = [];
+
+
+  /*public steppedChartLabels = ['Enero-2020','Febrero-2020','Marzo-2020','Abril-2020','Mayo-2020','Junio-2020','Julio-2020','Agosto-2020','Septiembre-2020','Octubre-2020','Noviembre-2020','Diciembre-2020'];
+  public steppedChartType = 'line';
+  public steppedChartLegend = true;
+  public steppedChartData = [];*/
+
   //Cosas Añadidas
   myControl = new FormControl();
   filteredOptions: Observable<string[]>;
@@ -47,6 +75,8 @@ export class GraphicsComponent implements OnInit {
   activos = [];
   activos_etq = [];
   activos_numero = [];
+  activos_fechas = [];
+  activos_mes_aux:string;
 
   ngOnInit(): void {
     this.bandera=true;
@@ -69,11 +99,60 @@ export class GraphicsComponent implements OnInit {
             this.activos_numero.push(0);
           }
           this.activos_numero[this.activos_etq.indexOf(this.activos[i].categoria)] ++;
+          
+          //this.activos_fechas[i]=this.activos[i].fecha[5];
+          //this.activos_fechas[i]+=this.activos[i].fecha[6];
+          this.activos_mes_aux=this.activos[i].fecha[5];
+          this.activos_mes_aux+=this.activos[i].fecha[6];
+
+          if(i==0){
+            for(let j = 0; j < this.activos.length; j++){
+              this.activos_fechas[j]=0;
+            }
+          }
+
+          if(this.activos_mes_aux=="01"){
+            this.activos_fechas[0]+=1;
+          }else if(this.activos_mes_aux=="02"){
+            this.activos_fechas[1]+=1;
+          }else if(this.activos_mes_aux=="03"){
+            this.activos_fechas[2]+=1;
+          }else if(this.activos_mes_aux=="04"){
+            this.activos_fechas[3]+=1;
+          }else if(this.activos_mes_aux=="05"){
+            this.activos_fechas[4]+=1;
+          }else if(this.activos_mes_aux=="06"){
+            this.activos_fechas[5]+=1;
+          }else if(this.activos_mes_aux=="07"){
+            this.activos_fechas[6]+=1;
+          }else if(this.activos_mes_aux=="08"){
+            this.activos_fechas[7]+=1;
+          }else if(this.activos_mes_aux=="09"){
+            this.activos_fechas[8]+=1;
+          }else if(this.activos_mes_aux=="10"){
+            this.activos_fechas[9]+=1;
+          }else if(this.activos_mes_aux=="11"){
+            this.activos_fechas[10]+=1;
+          }else if(this.activos_mes_aux=="12"){
+            this.activos_fechas[11]+=1;
+          }
+
+
         }
+        console.log(this.activos[0].fecha[5]);
+        console.log(this.activos[0].fecha[6]);
+        console.log(this.activos_fechas);
         console.log(this.activos_numero);
         console.log(this.activos_etq);
         this.doughnutChartLabels = this.activos_etq;
         this.doughnutChartData = this.activos_numero;
+
+       /* this.steppedChartLabels =['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+        this.steppedChartData = [1,2,3,4,5,6,7,8,9,10,11,12];*/
+        this.barChartData = [
+          { data: this.activos_fechas, label: 'Por Mes' },
+        ];
+        //this.steppedChartData=[this.activos_fechas];
         this.bandera=false;
         this.spinner();
       },
